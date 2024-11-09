0f8a456bff4ca77f9fad1d8829a3405470d6f855
VityaKurgaev <vitja.kurgaev@gmail.com>
2024-11-09 11:03:46 +0300

    091124 1153
    Vitya Kurgaev Set2

---
+++ Set2/Set2.cpp
@@ -0,0 +1,17 @@
+/*
+Set 2
+student - Vitya Kurgaev
+09112024 1059
+*/
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int a = 5;
+    int b = 2;
+    a = a + 1;
+    int result = a - b;
+    cout << result << endl;
+    return 0;
+}
