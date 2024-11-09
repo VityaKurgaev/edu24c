6f910911e028498096f4d9d3f327be47c8c8d710
VityaKurgaev <vitja.kurgaev@gmail.com>
2024-11-09 11:26:07 +0300

    091124 1125
    Vitja Kurgaev Set0C

---
+++ set0C/set0C.cpp
@@ -0,0 +1,20 @@
+/*
+Set 0C
+student - Vitya Kurgaev
+09112024
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for(int a = 0; a <= 4; a += 1)
+    {
+       for (int j = 1; j <= 5; j += 1)
+       {
+        cout << "*";
+       }
+        cout << endl; 
+    } 
+    return 0;
+}
