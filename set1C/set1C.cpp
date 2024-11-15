930b88b4237c9076bb0817241915ac8074369f5b
VityaKurgaev <vitja.kurgaev@gmail.com>
2024-11-15 16:01:34 +0300

    15112024
    Vitya Kurgaev

---
+++ set1C/set1C.cpp
@@ -0,0 +1,20 @@
+/*
+Set 1C
+student - Vitya Kurgaev
+15112024
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for(int a = 1; a <= 5; a += 1)
+    {
+       for (int j = 1; j <= 5; j += 1)
+       {
+        cout << j;
+       }
+        cout << endl; 
+    } 
+    return 0;
+}
