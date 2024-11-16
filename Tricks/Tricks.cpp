9a5f40004245ca7fbd82ba50a62e8e7e5df4f760
VityaKurgaev <vitja.kurgaev@gmail.com>
2024-11-16 10:39:54 +0300

    16112024
    Vitya Kurgaev

---
+++ Tricks/Tricks.cpp
@@ -0,0 +1,20 @@
+/*
+Tricks
+student - Vitya Kurgaev
+16112024
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define omg cout << "OMG!" << "\n";
+int32_t main()
+{
+    int x = 45;
+    int y = 54;
+
+    int z = (x < y) ? x : y;
+
+    cout << z << endl;
+    omg;
+
+    return 0;
+}
