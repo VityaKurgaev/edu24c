5d0f06689732d245a050d171281a5b619fdb5c18
VityaKurgaev <vitja.kurgaev@gmail.com>
2024-12-14 10:43:01 +0300

    14122024
    Vitya Kurgaev

--- Set1Struct/Set1Struct.cpp
+++ Set1Struct/Set1Struct.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 int result;
 
+
+
 struct student
 {
     string name;
@@ -10,7 +12,7 @@ struct student
 
 };
 
-student newStudent [] = 
+student Student [] = 
 { 
     {"Misha", 7, 4}, 
     {"Vasy", 12, 4}, 
@@ -19,21 +21,58 @@ student newStudent [] =
     {"ivan", 1, 3} 
 };
 
-int arr[5] = {4, 4, 5, 4, 3};
-
-
-
-for (int i = 0; i < 5; i++)
+void printnStudetSet1(Student *s)
 {
-    if arr[i] > arr[i + 1]
+    for(int i = 0; i < 5; ++i)
+    {
+        cout << s[i].name << endl;
+        cout << s[i].groupNumber << endl;
+        cout << s[i].averageGrade << endl;
+    }
 }
+
+void printnStudentsSet2(Student *s)
+{   
+    for (int i = 0; i < 3; ++i)
+    {
+        cout << s[i].name << endl;
+        cout << s[i].groupNumber << endl;
+        cout << s[i].averageGrade << endl;
+    }
+
+    double arr[3] = {};
+    for (int j = 0; j < 3; j++)
+    {
+        arr[j] = s[j].averageGrade;
+    }
+
+    double buffer{};
+    for (int k = 0; k < 3; k++)
+    {
+        for (int kk = 0; kk < 3; kk++)
+        {
+            if (arr[kk] < arr[kk + 1])
+            {
+                buffer = arr[kk];
+                arr[kk] = arr[kk + 1];
+                arr[kk + 1] = buffer;
+            }
+        }
+    }
+
+    for (int l = 0; l < 3; l++)
+    {
+        cout << arr[l] << " ";
+    }
+}
+
 
 
 void total_score()
 {
     for (int j = 0; j < 5; j++)
     {
-        result = result + newStudent[j].averageGrade;
+        result = result + Student[j].averageGrade;
 
     }
 
@@ -42,6 +81,7 @@ void total_score()
 
 int main()
 {
+    printnStudetnSet2(Student);
     total_score();
     return 0;
 }
