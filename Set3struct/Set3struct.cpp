0ba24186b4b1e71a57497ca7551061353ca00440
VityaKurgaev <vitja.kurgaev@gmail.com>
2025-01-11 11:14:03 +0300

    11012025
    Vitja Kurgaev

---
+++ Set3struct/Set3struct.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Student
+{
+    string fullName;
+    int groupNamber;
+    string faculty;
+};
+
+Student allStudentMGY[]
+{
+    {"Golovina", 103, "it security"},
+    {"Ryabkov", 103, "it security"},
+    {"Astaykin", 103, "it security"},
+    {"Kurgaev", 101, "hogwarts"},
+    {"Emelin", 110, "hogwarts"}
+
+};
+
+void studentSearch()
+{
+    while(1)
+    {
+        bool state;
+        string textUser;
+
+        cin >> textUser;
+
+        for (Student st : allStudentMGY)
+        {
+           if (cout << st.fullName.compare(textUser) == 0)
+           {
+                state = true;
+                cout << st.fullName << " " << st.groupNamber << " " << st.faculty << endl; break;
+           }
+           else state = false;
+        }
+        if (state == false) cout << "Please re-enter data!" << endl;
+    }
+}
+
+int main()
+{
+    studentSearch();
+    return 0;
+}
